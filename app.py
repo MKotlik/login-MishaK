5cb4ebbeb39d8e9c65f12839693566594633794b
Mikhail Kotlik <mkotlik@stuy.edu>
2016-09-29 14:50:52 -0400

    started login project

---
+++ app.py
@@ -0,0 +1,12 @@
+from Flask import flask, render_template, request
+
+app = Flask(__name__)
+
+username = "admin"
+password = "123456"
+
+@app.route("/")
+def index():
+
+@app.route("/authenticate/")
+def authenticate()
