a0dec8790d5d749b110b0c9963d68203e931f5c1
Misha Kotlik <mkotlik@stuy.edu>
2016-09-30 01:35:06 -0400

    Finished auth code in app.py

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ password = "123456"
 
 @app.route("/")
 def index():
+	return "Index"
 
 @app.route("/authenticate/")
-def authenticate()
+def authenticate():
+	if request.method == 'GET':
+		return render_template('auth.html', status=0)
+	else:
+		if (request.form['user'] == 'admin' 
+		and request.form['pass'] == '123456'):
+			return render_template('auth.html', status=1)
+		else:
+			return render_template('auth.html', status=2)
+	
+app.debug = True #enable debugging
+app.run()
