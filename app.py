8ce36c79ad71faa215f5cf4561852de33b570662
Misha Kotlik <mkotlik@stuy.edu>
2016-09-30 06:38:19 -0400

    Finished and tested site

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from Flask import flask, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-username = "admin"
-password = "123456"
-
 @app.route("/")
 def index():
-	return "Index"
+	return render_template('index.html')
 
-@app.route("/authenticate/")
+@app.route("/auth/", methods=['GET', 'POST'])
 def authenticate():
 	if request.method == 'GET':
 		return render_template('auth.html', status=0)
