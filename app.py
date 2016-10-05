a53bb450ccee0b2df347b001e7b51ae3f49a3a86
Misha Kotlik <mkotlik@stuy.edu>
2016-10-05 06:37:51 -0400

    Finished app.py

--- app.py
+++ app.py
@@ -18,17 +18,31 @@ def index():
 def authenticate():
 	if request.method == 'GET': #GET request
 		return render_template('auth.html', status='GET')
+		
 	else: #POST request, form submission
+		#Get the username & password entered
+		entered_name = request.form['user']
+		entered_pass = request.form['pass']
+		
 		if (request.form['auth_action'] == 'Register'): #Register attempt
 			#Check that names are clean (no , or " or ')
-			#Call register() func to ensure uniqueness and register
-		else: #Assume login
-			#Do login procedure here
-		if (request.form['user'] == 'admin' 
-		and request.form['pass'] == '123456'):
-			return render_template('auth.html', status=1)
-		else:
-			return render_template('auth.html', status=2)
+			if not is_input_clean(entered_name, entered_pass):
+				return render_template('auth.html', status='reg_bad_char')
+			#Name is clean, proceed to register
+			if register(entered_name, entered_pass) == True:
+				#Account created successfully, promppt login
+				return render_template('auth.html', status='reg_OK')
+			else:
+				#Account already exists, prompt login
+				return render_template('auth.html', status='reg_exists')
+				
+		else: #Login attempt
+			if login(entered_name, entered_pass) == True:
+				#Successful login, show msg
+				return render_template('auth.html', status='login_OK')
+			else:
+				#Failed login, prompt retry
+				return render_template('auth.html', status='login_Fail')
 
 #is_input_clean(username, password)
 #Returns false if either name contains , or " or '
@@ -40,8 +54,8 @@ def is_input_clean(username, password):
 		return True; #input is clean
 			
 #register(username, password)
-#Statuses: 'exists' - account already exists
-#'OK' - registration successful
+#Statuses: False - account already exists
+#True - registration successful
 def register(username, password):
 	#Open file in append mode for account checking/adding:
 	user_store = open('data/user_store.csv', 'a')
@@ -49,13 +63,29 @@ def register(username, password):
 	#Check uniqueness
 	for entry in user_store:
 		if entry.startswith(username):
-			return 'exists' #Account exists
+			return False #Account exists
 			
 	#Username is unique, proceed to store
 	#TODO: In the future, salt before hashing!!! Better yet, use an hmac!
 	user_store.write(username + ',' + hashlib.sha256(password).hexdigest())
 	user_store.close()
-	return 'OK' #signify successful registration
+	return True #signify successful registration
+	
+#login(username, password)
+#Statuses: False - no such account or password mismatch
+#True - successfully authenticated
+def login(username, password):
+	#Open file in append mode for account checking:
+	user_store = open('data/user_store.csv', 'r')
+	
+	for entry in user_store:
+		if entry.startswith(username):
+			#return whether given password matches given username
+			return entry[len(username):] == hashlib.sha256(password).hexdigest()
+			
+	#No account found with this username
+	return False
+	
 	
 app.debug = True #enable debugging
 app.run()
